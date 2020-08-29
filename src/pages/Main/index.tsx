14a99abec30fbc0b46097c6c40ff20817330f384
Lucas <lucasrocha446@gmail.com>
2020-08-29 11:14:55 -0300

    resolve conflicts

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -24,7 +24,7 @@ const Main: React.FC = () =>  {
                 <Wellcome />
             </LinearGradientStyled>
         </Container>
-        </ThemeProvider>
+      </ThemeProvider>
     );
 }
 
