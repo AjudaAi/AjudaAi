448f596145f1806b79c5f9ac8136cc15fe41be4f
Marcos  Oliveira <marcos.oliveira@fortbrasil.com.br>
2020-08-30 15:25:35 -0300

    resolve conflicts

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 
 import { ThemeProvider } from 'styled-components';
@@ -10,22 +10,34 @@ import theme from '../../styles/theme';
 
 import { Container, LinearGradientStyled } from './styles';
 
-const Main: React.FC = () =>  {
-    return(
-      <ThemeProvider theme={theme}>
-        <Container>
-            <LinearGradientStyled colors={theme.colors.gradientBackgroundColor}>
-                <StatusBar
-                    backgroundColor={theme.colors.sideBarColor}
-                    barStyle="default"
-                    translucent
-                />
-                {/* <SplashScreen /> */}
-                <Wellcome />
-            </LinearGradientStyled>
-        </Container>
-      </ThemeProvider>
-    );
-}
+const Main: React.FC = () => {
+  const [currentStepPages, setCurrentStepPages] = useState(0);
+  const [stepsComponents, setStepsComponents] = useState([
+    <SplashScreen />,
+    <Wellcome />,
+  ]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCurrentStepPages(1);
+    }, 3000);
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <LinearGradientStyled colors={theme.colors.gradientBackgroundColor}>
+          <StatusBar
+            backgroundColor={theme.colors.sideBarColor}
+            barStyle="default"
+            translucent
+          />
+          <SplashScreen />
+          {/* {stepsComponents[currentStepPages]} */}
+        </LinearGradientStyled>
+      </Container>
+    </ThemeProvider>
+  );
+};
 
 export default Main;
