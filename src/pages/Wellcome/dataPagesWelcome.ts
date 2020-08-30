a7e5e46029096a87b206c5dbddfcc60e635b708a
Lucas <lucasrocha446@gmail.com>
2020-08-30 17:36:46 -0300

    edit feature

---
+++ src/pages/Wellcome/dataPagesWelcome.ts
@@ -0,0 +1,57 @@
+import profileOla from '../../assets/ola.png';
+import profileVamosComecar from '../../assets/vamosComecar.png';
+import profileError from '../../assets/digitouClicouErrado.png';
+import profileOla from '../../assets/ola.png';
+import profileOla from '../../assets/ola.png';
+import profileOla from '../../assets/ola.png';
+
+export default [
+  {
+    title: 'Assistente, Ajuda aí',
+    subtitle: 'Apoio por voz',
+    profile: profileOla,
+    textCard: null,
+    buttonText1: null,
+    buttonText2: null,
+  },
+  {
+    title: 'Assistente, Ajuda aí',
+    subtitle: 'Apoio por voz',
+    profile: profileVamosComecar,
+    textCard: 'Olá, tudo bem? Seja bem-vindo. Estou aqui para te ajudar!',
+    buttonText1: null,
+    buttonText2: null,
+  },
+  {
+    title: 'Assistente, Ajuda aí',
+    subtitle: 'Apoio por voz',
+    profile: profileError,
+    textCard: 'Para me responder, basta tocar nos botões brancos abaixo.',
+    buttonText1: null,
+    buttonText2: null,
+  },
+  {
+    title: 'Assistente, Ajuda aí',
+    subtitle: 'Apoio por voz',
+    profile: profileOla,
+    textCard: 'Vamos lá, experimente!',
+    buttonText1: 'Clique Aqui',
+    buttonText2: null,
+  },
+  {
+    title: 'Assistente, Ajuda aí',
+    subtitle: 'Apoio por voz',
+    profile: profileOla,
+    textCard,
+    buttonText1,
+    buttonText2,
+  },
+  {
+    title: 'Assistente, Ajuda aí',
+    subtitle: 'Apoio por voz',
+    profile: profileOla,
+    textCard,
+    buttonText1,
+    buttonText2,
+  }
+]
